ddfe377bd3a0a8936c05207694fa8ec3aa04dd84
neruneruna7 <neruneruna7@gmail.com>
2024-02-02 15:56:50 +0900

    add: test機能とゲストからのqemuの終了

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    // QEMUは終了ステータスを(value << 1) | 1にして終了する
+    // 0はqemuの標準終了コードになるので使わないらしい
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // 0xf4はisa-debug-exitデバイスのiobase
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -13,12 +45,18 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    print!("Hello again!! i am writing from Rust! HaHa!! We are Oxide!! yehhhhhhhhhhhhhhhhhh!!!!");
-    println!(", some numbers: {} {}", 42, 1.337);
-
     println!("Hello World{}", "!");
 
-    panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    // trivialは些末なという意味みたい
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
