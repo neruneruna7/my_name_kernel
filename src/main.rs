e8479d202344aa65ec35c13f222faf275d24d102
neruneruna7 <neruneruna7@gmail.com>
2024-02-02 15:19:21 +0900

    add: printマクロの実装

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,12 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-    vga_buffer::print_something();
+    print!("Hello again!! i am writing from Rust! HaHa!! We are Oxide!! yehhhhhhhhhhhhhhhhhh!!!!");
+    println!(", some numbers: {} {}", 42, 1.337);
 
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
+    println!("Hello World{}", "!");
 
     loop {}
 }
