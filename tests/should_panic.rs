75920612aa8778652cd2df1ee426494bd69a0b35
neruneruna7 <neruneruna7@gmail.com>
2024-02-02 17:59:29 +0900

    add: ハーネスのないテスト

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,30 +1,33 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
+// #![feature(custom_test_frameworks)]
+// #![test_runner(test_runner)]
+// #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use wos_os_n71::{exit_qemu, serial_println, QemuExitCode};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    // test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    // ...これ，複数のテストを作れなくない？
-    // やっぱそうらしい
+// pub fn test_runner(tests: &[&dyn Fn()]) {
+//     serial_println!("Running {} tests", tests.len());
+//     for test in tests {
+//         test();
+//         serial_println!("[test did not panic]");
+//         exit_qemu(QemuExitCode::Failed);
+//     }
+//     // ...これ，複数のテストを作れなくない？
+//     // やっぱそうらしい
 
-    exit_qemu(QemuExitCode::Success);
-}
+//     exit_qemu(QemuExitCode::Success);
+// }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -35,7 +38,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
+// #[test_case]
 fn should_fail() {
     use wos_os_n71::serial_print;
 
